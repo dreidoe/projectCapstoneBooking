08c9e81ff88469db87b6463ecb71ce12adf1d3e9
Aundrei Bryant <drei97@gmail.com>
2023-01-29 21:07:50 -0600

    created: routes for controller methods

---
+++ app/merchant/routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import Merchant from "Merchant.js";
+
+const router = new Router();
+
+export default router;
--- app/user/routes.js
+++ app/user/routes.js
@@ -1,10 +1,1 @@
-import { Router } from "express";
-import merchant
 
-const router = mew Router();
-
-
-
-
-
-export default router;
