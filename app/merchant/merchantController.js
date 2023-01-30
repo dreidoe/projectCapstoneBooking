d12569a759c3e60f8ee9938f8410503d103c1faf
Aundrei Bryant <drei97@gmail.com>
2023-01-29 21:05:01 -0600

    created: methods for updating and deleting WIP

--- app/merchant/merchantController.js
+++ app/merchant/merchantController.js
@@ -1,22 +1,59 @@
 import mongoose from "mongoose";
 import config from "../config.js";
+import Merchant from "./Merchant.js";
+mongoose.set("strictQuery", false);
 
 mongoose
-.connect(config.dbConn)
-.then(() => {
-  console.info("Connected to the database");
-})
-.catch((err) => {
-  console.error("Error connecting to the database", err.message);
-});
-
-// create a method to add a update merchant by username
-
-updateMerchantByUserName(username, updatedMerchant) {
-  return Merchant.updateOne({
-    username,
-    updatedMerchant,
-    returnDocument: "after",
-    returnValidators: true,
+  .connect(config.dbConn)
+  .then(() => {
+    console.info("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database", err.message);
   });
-},
+
+const merchantController = {
+  // GET all Merchants
+  getAllMerchants() {
+    return Merchant.find({});
+  },
+  show(id) {
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      return Merchant.findById(id);
+    }
+
+    // Wrap the error in a rejected promise so that it can be CAUGHT.
+    return Promise.reject(new Error("Invalid ID"));
+  },
+
+  // CREATE a new Merchant
+  createMerchant(merchant) {
+    return Merchant.create(merchant);
+  },
+  // Show a Merchant by username
+  showByUserName(username) {
+    return Merchant.findOne({ username });
+  },
+  // UPDATE a Merchant
+  updateByUserName(username, updatedMerchant) {
+    return Merchant.updateOne({
+      username,
+      updatedMerchant,
+      returnDocument: "after",
+      returnValidators: true,
+    });
+  },
+  // DELETE a Merchant
+  deleteById(id2Delete) {
+    if (mongoose.Types.ObjectId.isValid(id2Delete)) {
+      return Merchant.findByIdAndDelete(id2Delete);
+    }
+    return Promise.reject(new Error("Invalid ID"));
+  },
+  // DELETE a Merchant by username
+  deleteByUserName(username) {
+    return Merchant.findOneAndDelete({ username });
+  },
+};
+
+export default merchantController;
