95d3023e6b86da6333102900b96d9eda533e7372
Aundrei Bryant <drei97@gmail.com>
2023-01-29 21:09:05 -0600

    Started: creating get methods for users

--- app/user/userController.js
+++ app/user/userController.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import config from "../merchant/config";
-import User from "./user.js";
-mongoose.set(strictQuery, false);
+import config from "../config";
+import User from "./User.js";
+mongoose.set("strictQuery", false);
 
 // Connect to the database
 mongoose
@@ -12,3 +12,20 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to the database", err);
   });
+
+const userController = {
+  // Get all users
+  getAllUsers() {
+    return User.find({});
+  },
+  show(id) {
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      return User.findById(id);
+    }
+
+    // Wrap the error in a rejected promise so that it can be CAUGHT.
+    return Promise.reject(new Error("Invalid ID"));
+  },
+};
+
+export default userController;
