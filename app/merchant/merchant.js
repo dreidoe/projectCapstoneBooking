37c346be0ec05ba8463053d61b026f3f4c147b50
Aundrei Bryant <drei97@gmail.com>
2023-01-29 19:37:35 -0600

    refactored: user and merchant schema

--- app/merchant/merchant.js
+++ app/merchant/merchant.js
@@ -1,40 +1,52 @@
 import { Schema, model } from "mongoose";
 
-export default model(
-  "Merchant",
-  new Schema({
-    firstName: { type: String, required: true },
-    lastName: {type: String, required: true},
-    userName: {
-      type: String,
-      required: [true, "Username is required"],
-      unique: [true, "Username already exists"],
-      minLength: [3, "Username must be at least 3 characters long"],
-      maxLength: [15, "Username must be at most 20 characters long"],
-    },
-    email: {type: String, required: true},
-    password: {type: String, required: [true, "Password is required"],
-    minLength:[12, "Password must be at least 12 characters long"],
-    },
+const merchantSchema = new Schema({
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: [true, "Username already exists"],
+    minLength: [3, "Username must be at least 3 characters long"],
+    maxLength: [20, "Username cannot exceed 20 characters"],
+  },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [
+      12,
+      "Password must be at least 12 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character",
+    ].regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/),
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
 
-    avatar: { type: String, required: false },
-    id: { type: String, required: true },
-  })
-);
+  avatar: { type: String, required: false, default: "images/profilePic.png" },
+  id: { type: String, required: true },
+});
 
-export client  model(
-  "Client",
-  new Schema({
-    fullName: { type: String, required: true },
-    userName: {
-      type: String,
-      required: [true, "Username is required"],
-      unique: [true, "Username already exists"],
-      minLength: [3, "Username must be at least 3 characters long"],
-      maxLength: [15, "Username must be at most 20 characters long"],
-    },
+const userSchema = new Schema({
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: [true, "Username already exists"],
+    minLength: [3, "Username must be at least 3 characters long"],
+    maxLength: [20, "Username cannot exceed 20 characters"],
+  },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: [
+      12,
+      "Password must be at least 12 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character",
+    ].regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/),
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
 
-    avatar: { type: String, required: false },
-    id: { type: String, required: true },
-  })
-);
+  avatar: { type: String, required: false, default: "images/profilePic.png" },
+  id: { type: String, required: true },
+});
