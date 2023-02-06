b0fec56de6fed94573a9f25a8042dd2ca44ee37c
Aundrei Bryant <drei97@gmail.com>
2023-02-06 11:55:27 -0600

    refactor:  connections stored here now

--- app/config.js
+++ app/config.js
@@ -7,4 +7,8 @@ dotenv.config();
 
 export default {
   dbConn: process.env.DB_CONN,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1h",
+  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT || 3000,
+  saltRounds: Number(process.env.SALT_ROUNDS) || 10,
 };
